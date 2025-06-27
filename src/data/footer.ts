54bcec30f75c8c847437516a51d569c8ae2730b5
Mishawn Lolis <mishawn.lolis@rallysphere.com>
2025-06-27 13:25:01 -0500

    Updates

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -7,7 +7,7 @@ export const footerDetails: {
     telephone: string;
     socials: ISocials;
 } = {
-    subheading: "Empowering businesses with cutting-edge financial technology solutions.",
+    subheading: "Empowering players and clubs with cutting-edge technology solutions.",
     quickLinks: [
         {
             text: "Features",
